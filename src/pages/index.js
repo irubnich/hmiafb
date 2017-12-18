e09fcba38c7292be10896596d13046121e9553fe
Ilya Rubnich <irubnich@gmail.com>
2017-12-18 11:21:19 -0500

    Styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,58 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components';
 
-import './index.css'
+const Wrapper = styled.div`
+  margin-top: 100px;
+  font-family: sans-serif;
+  text-align: center;
+  font-size: 45px;
+`
+
+const Title = styled.div`
+  font-size: 60px;
+`
+
+const Price = styled.div`
+  margin-top: 40px;
+`
+
+const LastUpdated = styled.div`
+  margin-top: 6px;
+  font-size: 10px;
+`
+
+const BuyLink = styled.div`
+  margin-top: 40px;
+  font-size: 18px;
+`
+
+const Donate = styled.div`
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
+  font-size: 10px;
+`
 
 const IndexPage = () => (
-  <div>
-    <div id="whatis">WHAT'S THE FUCKING<br />PRICE OF BITCOIN?</div>
-    <div id="price">
+  <Wrapper>
+    <Title>WHAT'S THE FUCKING<br />PRICE OF BITCOIN?</Title>
+    <Price>
       <span id="expletive">WAIT...</span>
       <br />
       <select></select>
       <span id="the-price"></span>
-    </div>
-    <div id="last-updated">Last updated: <span></span></div>
+    </Price>
+    <LastUpdated>Last updated: <span></span></LastUpdated>
 
-    <div id="buy">
+    <BuyLink>
       <a href="https://www.coinbase.com/join/irubnich">GO BUY SOME</a>
-    </div>
+    </BuyLink>
 
-    <div id="donate">
+    <Donate>
       1Dk8AnCLEPWCAAau8Tyb5BPTPBf9Zx1Yw4 &lt;3
-    </div>
-  </div>
+    </Donate>
+  </Wrapper>
 )
 
 export default IndexPage
