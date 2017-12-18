541060422c3c3a5c022d82170656159400ef3110
Ilya Rubnich <irubnich@gmail.com>
2017-12-18 14:18:41 -0500

    cleanup

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
+// Firebase
+const functions = require('firebase-functions')
+
+// Libraries
 const axios = require('axios')
 const cors = require('cors')({ origin: true })
-const functions = require('firebase-functions')
 
 exports.getExchangeRates = functions.https.onRequest((req, res) => {
   axios.get("https://coinbase.com/api/v1/currencies/exchange_rates").then(coinbaseResponse => {
