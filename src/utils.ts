49fd9d6ec881ee2c148c2dde8d3194ac502b18ed
Ilya Rubnich <irubnich@gmail.com>
2020-05-13 12:55:12 -0400

    Convert easy parts to TS

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,11 @@
 // Converts a float into a "formtted price", e.g "18000.00" => "18,000.00"
-const toPrice = (price) => {
+export const toPrice = (price: number): string => {
   return price.valueOf().toFixed(2).replace(/./g, (c, i, a) => {
     return i && c !== "." && !((a.length - i) % 3) ? "," + c : c;
   })
-}
+};
 
 // Gets a random element from an array
-const randomElement = (array) => {
-  return array[Math.floor(Math.random() * array.length)]
-}
-
-
-module.exports = {
-  toPrice: toPrice,
-  randomElement: randomElement
-}
+export const randomElement = <T>(array: Array<T>): T => {
+  return array[Math.floor(Math.random() * array.length)];
+};
